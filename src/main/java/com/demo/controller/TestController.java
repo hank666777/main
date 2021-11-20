73a8188de18ef91eddbecb07d00bcf7b17333444
hank666777 <fengptt47@gmail.com>
2021-11-20 23:55:37 +0800

    no message

--- src/main/java/com/demo/controller/TestController.java
+++ src/main/java/com/demo/controller/TestController.java
@@ -2,14 +2,21 @@ package com.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@RequestMapping("/")
+@Slf4j
+@RequestMapping("/api")
 @Controller
 public class TestController {
 
+    @ResponseStatus(code = HttpStatus.OK, reason = "OKKKKK")
+    @ResponseBody
     @GetMapping("/test")
     public ResponseEntity<?> TestController(){
         return new ResponseEntity<>("test OK", HttpStatus.OK);

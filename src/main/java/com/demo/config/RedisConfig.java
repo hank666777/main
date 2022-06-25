c123423d34e5d8466966bc4d0878085e6b7ce564
hank666777 <fengptt47@gmail.com>
2022-06-25 13:25:00 +0800

    no message

--- src/main/java/com/demo/config/RedisConfig.java
+++ src/main/java/com/demo/config/RedisConfig.java
@@ -3,7 +3,7 @@ package com.demo.config;
 public class RedisConfig {
 
     public void test(){
-        System.out.println();
+        System.out.println("commit");
     }
     public void test2(){
         
